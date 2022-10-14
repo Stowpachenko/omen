7552fef2fd9853a1bf3694bca3adc0cc373a9335
Stowpachenko <dnepro.com@gmail.com>
2022-10-14 20:32:28 +0300

    fixes

--- js-dev/scipt.js
+++ js-dev/scipt.js
@@ -2,4 +2,29 @@ document.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad() {
 
+    const select = document.getElementById('select');
+    const navList = document.getElementById('nav-list');
+    const navListButton = document.getElementById('nav-list-button');
+
+    let options = {
+        clikableElem: select,
+        classElem: select,
+        className: "active"
+    };
+    toggleSelect( {
+        clikableElem: select,
+        classElem: select,
+        className: "active"
+    } );
+    toggleSelect({
+        clikableElem: navListButton,
+        classElem: navList,
+        className: "open"
+    });
+
+    function toggleSelect(options) {
+        options.clikableElem.addEventListener('click', () => {
+            options.classElem.classList.toggle(options.className);
+        });
+    }
 }
